b1491f7e28e4daaaf5b35075f085b5227289d92c
vlifanova <vlifanova2@alfabank.ru>
2023-04-07 23:46:55 +0300

    Test Container

--- src/main/java/ru/netology/conditional/config/JavaConfig.java
+++ src/main/java/ru/netology/conditional/config/JavaConfig.java
@@ -1,13 +1,16 @@
 package ru.netology.conditional.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import ru.netology.conditional.profile.DevProfile;
 import ru.netology.conditional.profile.ProductionProfile;
 import ru.netology.conditional.profile.SystemProfile;
 
 @Configuration
+@ComponentScan("ru.netology.conditional.profile")
 public class JavaConfig {
     @Bean(name = "devProfile")
     @ConditionalOnExpression("${properties.netology.profile.dev:true}")
@@ -21,9 +24,9 @@ public class JavaConfig {
 
     @Bean(name = "productionProfile")
     @ConditionalOnExpression("${properties.netology.profile.dev:false}")
-//    @ConditionalOnProperty(
-//            value = "netology.profile.dev",
-//            havingValue = "false")
+   /* @ConditionalOnProperty(
+            value = "netology.profile.dev",
+            havingValue = "false")*/
     public SystemProfile prodProfile() {
         return new ProductionProfile();
     }
--- src/main/java/ru/netology/conditional/controller/ProfileController.java
+++ src/main/java/ru/netology/conditional/controller/ProfileController.java
@@ -1,5 +1,7 @@
 package ru.netology.conditional.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,7 @@ import ru.netology.conditional.profile.SystemProfile;
 public class ProfileController {
     private SystemProfile profile;
 
-    public ProfileController(SystemProfile profile) {
+    public ProfileController( SystemProfile profile) {
         this.profile = profile;
     }
 
